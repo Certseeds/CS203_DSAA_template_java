9599cd8b331af1fd305efd0a121d5d8ae611350c
Certseeds <51754303+Certseeds@users.noreply.github.com>
2020-07-30 19:26:03 +0800

    添加readme.

--- test/lab_00/lab_00_A_Test.java
+++ test/lab_00/lab_00_A_Test.java
@@ -34,31 +34,31 @@ public class lab_00_A_Test {
     @Order(0)
     @Test
     public void test_0() {
-        for (int i = 0; i < 114; i++) {
-            int x = random.nextInt(0x3f3f3f3f);
-            int y = random.nextInt(0x3f3f3f3f);
-            assertEquals(x + y, lab_00_A.cal(new int[]{x, y}));
-        }
+        assertEquals(0xFFFFFFFF, lab_00_A.cal(new int[] { 0xFFFFFFFE, 1 }));
+        assertEquals(0x00000001, lab_00_A.cal(new int[] { 0x00000000, 1 }));
+        assertEquals(0xFFFFFFF0, lab_00_A.cal(new int[] { 0xFFFFFFEF, 1 }));
     }
 
     @Order(1)
     @Test
-    public void test_2() throws IOException {
-        redirect.set_path("01.data.in", "01.test.out");
-        lab_00_A.output(lab_00_A.cal(lab_00_A.read()));
-        Pair<String, String> p = redirect.compare_double("01.test.out", "01.data.out");
-        assertEquals(p.getFirst().length(), p.getSecond().length());
-        assertEquals(p.getFirst(), p.getSecond());
-
-
-        redirect.set_path("01.data.in", "01.test.out");
-        lab_00_A.output(lab_00_A.cal(lab_00_A.reader()));
-        p = redirect.compare_double("01.test.out", "01.data.out");
-        assertEquals(p.getFirst().length(), p.getSecond().length());
-        assertEquals(p.getFirst(), p.getSecond());
+    public void test_1() {
+        assertEquals(0xFFFFFFFE, lab_00_A.cal(new int[] { 0xFFFFFFFF, -1 }));
+        assertEquals(0xFFFFFFFE, lab_00_A.cal(new int[] { 0xFFFFFFFF, -1 }));
+        assertEquals(0xFFFFFFEE, lab_00_A.cal(new int[] { 0xFFFFFFEF, -1 }));
+        assertEquals(0x12345678 << 1, lab_00_A.cal(new int[] { 0x12345678, 0x12345678 }));
     }
 
     @Order(2)
+    @Test
+    public void test_2() {
+        for (int i = 0; i < 114; i++) {
+            int x = random.nextInt(0x3f3f3f3f);
+            int y = random.nextInt(0x3f3f3f3f);
+            assertEquals(x + y, lab_00_A.cal(new int[] { x, y }));
+        }
+    }
+
+    @Order(3)
     @Test
     public void test_3() throws IOException {
         // 测试
@@ -67,7 +67,6 @@ public class lab_00_A_Test {
 
         redirect.set_path("01.data.in");
         assertEquals(628, lab_00_A.cal(lab_00_A.reader()));
-
     }
 
     @AfterAll
@@ -77,4 +76,3 @@ public class lab_00_A_Test {
         System.out.println(String.format("cost %d ms", System.currentTimeMillis() - begin_time));
     }
 }
-
